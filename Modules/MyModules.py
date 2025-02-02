40125e6eb70e3007436dc57c15a1f945b959b388
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-02 23:16:01 +0530

    Create MyModules

---
+++ Modules/MyModules.py
@@ -0,0 +1,16 @@
+# Modules has usually function, variable.. file
+# Use for oranization code
+
+
+def greeting(name):
+    print("Hello " + name)
+
+def bye(name):
+    print("Good Bye " + name)
+
+# Dictionaires
+person1 = {
+    "name" : "Bob",
+    "age" : 27,
+    "country" : "USA"
+}
