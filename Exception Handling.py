064abebe63573461a10c3396437f39c66e8ac07f
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-08 23:47:14 +0530

    Exception Handling Dictionaires.py

---
+++ Exception Handling.py
@@ -0,0 +1,13 @@
+# trry:
+#   code we wont to run
+
+# except:
+#   code which run if an error occured
+
+try :
+    print(x)
+except :
+    print("And exception occures!") # SyntaxError: invalid syntax
+
+#------------------------------------------------------------------------------
+
