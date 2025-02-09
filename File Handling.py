40cc792410c88065ef29bd4519fdd5ecbd2498e7
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-09 23:42:19 +0530

    Create Read file in File Handling

--- File Handling.py
+++ File Handling.py
@@ -8,6 +8,22 @@
 fhand = open("C:Desktop/Phython/first.txt")
 print(fhand) # <_io.TextIOWrapper name='C:/Users/kanis/OneDrive/Desktop/Phython/first.txt' mode='r' encoding='cp1252'>
 
-print("Name of the file :" , fhand.name) # Name of the file : C:/Users/kanis/OneDrive/Desktop/Phython/first.txt
+print("Name of the file :" , fhand.name) # Name of the file : C:/Desktop/Phython/first.txt
 
 
+# Read File
+count = 0
+for line in fhand:
+    print(line.rstrip()) 
+    count += 1
+# Hello World
+# Testing....
+
+# Num of count
+print("Line Count ",count) # 2
+fhand.close() # Close the file
+# ----------------------------------------------------------------------------------
+
+#2.Read File
+print(fhand.read())
+fhand.close()
