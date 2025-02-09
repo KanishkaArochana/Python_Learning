efc0edaf4e39a0c89ca496b60814f5089b856135
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-09 23:40:50 +0530

    Create Open file in File Handling

---
+++ File Handling.py
@@ -0,0 +1,13 @@
+# 1.Open the file
+# 2.Read/ Write
+# 3.Close the file
+
+# Open the file
+       # File object = open(filename, mode)
+
+fhand = open("C:Desktop/Phython/first.txt")
+print(fhand) # <_io.TextIOWrapper name='C:/Users/kanis/OneDrive/Desktop/Phython/first.txt' mode='r' encoding='cp1252'>
+
+print("Name of the file :" , fhand.name) # Name of the file : C:/Users/kanis/OneDrive/Desktop/Phython/first.txt
+
+
