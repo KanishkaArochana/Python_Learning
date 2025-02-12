2d3bc0edc1ed88ca226d8c493cc7943fb8c52321
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-12 16:46:44 +0530

    Create Operater in Identity Operator.py

---
+++ Identity Operator.py
@@ -0,0 +1,14 @@
+# Operater
+# 1.is --> equal
+# 2.is not --> not equal
+
+x = 30
+
+print(30 is 30) #True
+print(10 is 30) #False
+
+print(30 is not 30) #False
+print(10 is not 30) #True
+
+# == --> Checking whether the 2 values ​​on both sides are equal or not
+# is --> Checking whether the 2 objects ​​on both sides are equal or not
