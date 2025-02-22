5d78e7382a99b3b76cd8bd0aeeafbd8f63a74bcc
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-22 22:56:40 +0530

    Create Allow Duplicates to List.py

--- List.py
+++ List.py
@@ -1,6 +1,9 @@
 # Python Lists
 
-#List
+# List
 thislist = ["apple", "banana", "cherry"]
 print(thislist)  # ['apple', 'banana', 'cherry']
 
+# Allow Duplicates
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist) # ['apple', 'banana', 'cherry', 'apple', 'cherry']
